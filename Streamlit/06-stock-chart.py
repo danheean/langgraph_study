3997e921272cddb3432a58b88f6ebbd8fefaa711
jurepi <danhee.an@gmail.com>
2024-07-20 14:44:26 +0900

    date_input with finance data 업로드

---
+++ Streamlit/06-stock-chart.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import FinanceDataReader as fdr
+import datetime
+
+# Finance Data Reader
+# https://github.com/financedata-org/FinanceDateReader
+
+date = st.date_input(
+    "조회 시작일을 선택해 주세요", 
+    datetime.datetime(2022, 1, 1)
+)
+
+code = st.text_input(
+    '종목코드', 
+    value = '',
+    placeholder = '종목코드를 입력해 주세요',
+)
+
+if code and date:
+    df = fdr.DateReader(code, date)
+    data = df.sort_index(ascending=True).loc[:, 'Close']
+    st.line_chart(data)
+    
